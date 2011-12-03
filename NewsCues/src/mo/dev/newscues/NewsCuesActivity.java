82c47f8db56aebb5b17011f1770677b45bf63d12
npruett <nathan.pruett@gmail.com>
2011-12-03 13:04:07 -0500

    pull feed, display text, JSON lib

--- NewsCues/src/mo/dev/newscues/NewsCuesActivity.java
+++ NewsCues/src/mo/dev/newscues/NewsCuesActivity.java
@@ -1,13 +1,56 @@
 package mo.dev.newscues;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NewsCuesActivity extends Activity {
+	private TextView textView;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        textView = (TextView) findViewById(R.id.text);
     }
+    
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+			String response = "";
+			for (String url : urls) {
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(url);
+				try {
+					HttpResponse execute = client.execute(httpGet);
+					InputStream content = execute.getEntity().getContent();
+
+					BufferedReader buffer = new BufferedReader(
+							new InputStreamReader(content));
+					String s = "";
+					while ((s = buffer.readLine()) != null) {
+						response += s;
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return response;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			textView.setText(result);
+		}
+	}
 }
---
+++ NewsCues/src/mo/dev/newscues/NewsCuesApplication.java
@@ -0,0 +1,7 @@
+package mo.dev.newscues;
+
+import android.app.Application;
+
+public class NewsCuesApplication extends Application {
+	public static final String TAG = "NewsCues";
+}
