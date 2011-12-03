24b6ef6887f73a58eca7b6c4c5d840bd3c8e9bab
npruett <nathan.pruett@gmail.com>
2011-12-03 10:17:08 -0500

    initial

---
+++ NewsCues/src/mo/dev/newscues/NewsCuesActivity.java
@@ -0,0 +1,13 @@
+package mo.dev.newscues;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewsCuesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
